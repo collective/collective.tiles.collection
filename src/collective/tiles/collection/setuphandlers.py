23b993defbb4593403f0f3ae54fac9fc126c6226
Maurits van Rees <maurits@vanrees.org>
2023-08-29 15:49:06 +0200

    Hide mosaicsupport module, and automatically install/uninstall it when needed.

--- src/collective/tiles/collection/setuphandlers.py
+++ src/collective/tiles/collection/setuphandlers.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.GenericSetup.tool import UNKNOWN
 from zope.interface import implementer
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
+
+    def getNonInstallableProducts(self):
+        return [
+            "collective.tiles.collection.mosaicsupport",
+        ]
 
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and Add'on installation screen"""
@@ -13,10 +19,21 @@ class HiddenProfiles(object):
             'collective.tiles.collection:uninstall',
         ]
 
+
 def post_install(context):
     """Post install script"""
+    # Note: context is the portal_setup tool.
+    if context.getLastVersionForProfile('plone.app.mosaic:default') == UNKNOWN:
+        return
+    context.runAllImportStepsFromProfile(
+        'collective.tiles.collection.mosaicsupport:default'
+    )
 
 
 def uninstall(context):
     """Uninstall script"""
-    # Do something at the end of the uninstallation of this package.
+    # Mark our optional mosaicsupport module as uninstalled.
+    # Works fine, even when it was not installed, or is not available.
+    context.unsetLastVersionForProfile(
+        'collective.tiles.collection.mosaicsupport:default'
+    )
